e67c1e548d3f622c0490214f4a0c82a603f1590f
Diajengsekar <diajengsekararum31@gmail.com>
2025-10-03 20:09:39 +0700

    prak 8 - Menambahkan halaman baru

--- lib/main.dart
+++ lib/main.dart
@@ -61,7 +61,7 @@ class _MyHomePageState extends State<MyHomePage> {
         page = GeneratorPage();
         break;
       case 1:
-        page = Placeholder();
+        page = FavoritesPage();   // ✅ Ganti Placeholder ke FavoritesPage
         break;
       default:
         throw UnimplementedError('no widget for $selectedIndex');
@@ -73,7 +73,7 @@ class _MyHomePageState extends State<MyHomePage> {
           children: [
             SafeArea(
               child: NavigationRail(
-                extended: constraints.maxWidth >= 600,  // ← Here.
+                extended: constraints.maxWidth >= 600,
                 destinations: [
                   NavigationRailDestination(
                     icon: Icon(Icons.home),
@@ -104,7 +104,6 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 }
-
 
 class GeneratorPage extends StatelessWidget {
   @override
@@ -175,6 +174,34 @@ class BigCard extends StatelessWidget {
           semanticsLabel: "${pair.first} ${pair.second}",
         ),
       ),
+    );
+  }
+}
+
+class FavoritesPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+
+    if (appState.favorites.isEmpty) {
+      return Center(
+        child: Text('No favorites yet.'),
+      );
+    }
+
+    return ListView(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(20),
+          child: Text('You have '
+              '${appState.favorites.length} favorites:'),
+        ),
+        for (var pair in appState.favorites)
+          ListTile(
+            leading: Icon(Icons.favorite),
+            title: Text(pair.asLowerCase),
+          ),
+      ],
     );
   }
 }
